4ad0d578d98cf89bf0876b76c2166f95a34abe5d
Jake Staab <jjstaab@mail.fhsu.edu>
2021-03-10 08:56:06 -0600

    update db info

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php 
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    $password = 'sesame';
+    $dsn = 'mysql:host=eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=g8wbc0twqar62fu9';
+    $username = 'nmcaz1rxzlfmyxno';
+    $password = 'ty2yef9ewuzv8gxs';
 
     try {
         $db = new PDO($dsn, $username, $password);
