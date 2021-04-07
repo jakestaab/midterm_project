a0c50f3d55ec6dff0d27e08c37c2982b8170c3d5
Jake Staab <jjstaab@mail.fhsu.edu>
2021-04-07 16:11:20 -0500

    changes to database

--- model/database.php
+++ model/database.php
@@ -1,13 +1,15 @@
 <?php 
-    /*
+    
     $dsn = 'mysql:host=eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=g8wbc0twqar62fu9';
     $username = 'nmcaz1rxzlfmyxno';
     $password = 'ty2yef9ewuzv8gxs';
-    */
+    
 
+    /*
     $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
     $username = 'root';
     $password = 'sesame';
+    */
 
     try {
         $db = new PDO($dsn, $username, $password);
